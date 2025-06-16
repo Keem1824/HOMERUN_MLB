eadba701d649d855c37293f31bd2a58b18080651
Keem1824 <apatterson2796@gmail.com>
2025-06-16 16:20:57 -0400

    Update live_roster.py

--- mlb_hr_predictor_pro_ultra/data/live_roster.py
+++ mlb_hr_predictor_pro_ultra/data/live_roster.py
@@ -2,9 +2,3 @@
 import sys
 sys.path.append("libs")
 from mlbstatsapi import MLBStatsAPI
-import pandas as pd
-
-api = MLBStatsAPI()
-
-def get_all_teams():
-    return [team['name'] for team in api.get_teams(sportId=1)['teams']]
