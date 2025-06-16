06a6d5315e3d4abffb0752112c024fbdddb1b3ac
Keem1824 <apatterson2796@gmail.com>
2025-06-16 16:03:32 -0400

    Add files via upload

--- mlb_hr_predictor_pro_ultra/app.py
+++ mlb_hr_predictor_pro_ultra/app.py
@@ -3,5 +3,5 @@ import streamlit as st
 from data.live_roster import get_all_teams
 
 teams = get_all_teams()
-st.title("✅ MLB StatsAPI Live Test")
-st.selectbox("Select an MLB team", teams)
+st.title("✅ MLB Team Picker (Live Roster Test)")
+st.selectbox("Select a team", teams)
